acd5260a3231eb453c6e90a7de9e20798e84ff63
Ali Momeni <aymomeni@gmail.com>
2018-03-27 14:25:15 -0600

    string permutation comparator method done

--- Cracking/src/chapter1/CheckPermutation.java
+++ Cracking/src/chapter1/CheckPermutation.java
@@ -1,5 +1,51 @@
 package chapter1;
 
+import java.util.HashMap;
+
+/**
+ * Given two strings, write a method to decide if one is a permutation of the other.
+ * @author Momeni
+ *
+ */
 public class CheckPermutation {
 
+	
+	/**
+	 * Checks if s1 and s2 are permutations of each other (case sensitive)
+	 * @param s1
+	 * @param s2
+	 * @return true/false
+	 */
+	boolean isPermutation(String s1, String s2){
+		
+		if(s1.length() != s2.length()){
+			return false;
+		}
+		
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for(char c : s1.toCharArray()){
+			if(map.containsKey(c)){
+				map.put(c, (map.get(c)+1));
+			} else {
+				map.put(c, 1);
+			}
+		}
+		
+		for(char c : s2.toCharArray()){
+			if(!map.containsKey(c)){
+				return false;
+			} else {
+				map.put(c, (map.get(c)-1));
+			}
+		}
+		
+		for(int i : map.values()){
+			if(i != 0){
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
