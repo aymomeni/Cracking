ad036a246e9b2a56c6b43daed2fcf54c96bd53d6
Ali Momeni <aymomeni@gmail.com>
2019-01-08 12:31:31 -0700

    validateBST should be working

---
+++ Cracking/src/chapter4/Tests/ValidateBSTTests.java
@@ -0,0 +1,52 @@
+package chapter4.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import chapter4.BiTreeNode;
+import chapter4.ValidateBST;
+
+public class ValidateBSTTests {
+
+	@Test
+	public void test() {
+		
+		ValidateBST v = new ValidateBST();
+		
+		BiTreeNode n = new BiTreeNode(15);
+		
+		n.leftChild = new BiTreeNode(7);
+		n.leftChild.leftChild = new BiTreeNode(2);
+		n.leftChild.rightChild = new BiTreeNode(9);
+		
+		n.rightChild = new BiTreeNode(17);
+		n.rightChild.leftChild = new BiTreeNode(16);
+		n.rightChild.rightChild = new BiTreeNode(19);
+	
+		boolean result = v.validateBst(n);
+		
+		assertTrue(result);
+	}
+	
+	@Test
+	public void test2() {
+		
+		ValidateBST v = new ValidateBST();
+		
+		BiTreeNode n = new BiTreeNode(15);
+		
+		n.leftChild = new BiTreeNode(7);
+		n.leftChild.leftChild = new BiTreeNode(2);
+		n.leftChild.rightChild = new BiTreeNode(9);
+		
+		n.rightChild = new BiTreeNode(17);
+		n.rightChild.leftChild = new BiTreeNode(16);
+		n.rightChild.rightChild = new BiTreeNode(14);
+	
+		boolean result = v.validateBst(n);
+		
+		assertFalse(result);
+	}
+
+}
---
+++ Cracking/src/chapter4/ValidateBST.java
@@ -0,0 +1,45 @@
+package chapter4;
+
+import java.util.LinkedList;
+
+public class ValidateBST {
+	
+	LinkedList<Integer> values = new LinkedList<Integer>();
+	boolean validate = true;
+	
+	public boolean validateBst(BiTreeNode root){
+		
+		if(root != null){	
+			validateBst(root.leftChild);
+			System.out.println(root.val + "LinkedListSize: " + values.size());
+			if(!checkLastValue(root)){
+				System.out.println("Got here too");
+				validate = false;
+			}
+			validateBst(root.rightChild);
+		}
+		
+		return validate;
+	}
+	
+	private boolean checkLastValue(BiTreeNode node){
+		
+		if(!values.isEmpty()){			
+			int lastVal= values.getLast();
+			System.out.println(" LastValue: " + lastVal + " CurrentVal: " + node.val);
+			if(lastVal < node.val){
+				values.addLast(node.val);
+				return true;
+			} else{
+				System.out.println("Got Here");
+				return false;
+			}
+		}
+		
+		values.addLast(node.val); // just for the first run
+		return true;
+		
+	}
+	
+	
+}
