5283861766c9442ec697df85c5896e936843cd43
Ali Momeni <aymomeni@gmail.com>
2019-02-13 11:45:13 -0700

    2d matrix for topological sort

--- Cracking/src/chapter4/AdjGraph.java
+++ Cracking/src/chapter4/AdjGraph.java
@@ -1,18 +1,5 @@
 package chapter4;
 
-/*
- * Represents an Graph using an Adjacency Matrix
- */
 public class AdjGraph {
 
-	int[][] graph;
-	public AdjGraph(int n){
-		graph = new int[n][n];
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++){
-				graph[i][j] = 0;
-			}
-		}
-	}
 }
