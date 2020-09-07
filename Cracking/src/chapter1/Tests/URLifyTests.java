752374e3d9983c31a04ac7c309423b583d1883af
aymomeni <aymomeni@gmail.com>
2020-09-07 17:46:56 -0600

    added urlify with char array

--- Cracking/src/chapter1/Tests/URLifyTests.java
+++ Cracking/src/chapter1/Tests/URLifyTests.java
@@ -2,9 +2,10 @@ package chapter1.Tests;
 
 import static org.junit.Assert.*;
 
+import java.nio.CharBuffer;
+
 import org.junit.Test;
 
-import chapter1.IsUnique;
 import chapter1.URLify;
 
 public class URLifyTests {
@@ -14,5 +15,13 @@ public class URLifyTests {
 		URLify uL = new URLify();
 		String sOut = uL.urlifyString("Mr John Smith    ");
 		assertEquals("Mr%20John%20Smith", sOut);
+	}
+	
+	@Test
+	public void test2() {
+		URLify uL = new URLify();
+		char[] sOut = uL.urlify(new char[]{'M','r',' ','J',' ','S',' ', ' ', ' ', ' '});
+		System.out.println(sOut);
+		assertEquals(CharBuffer.wrap(new char[]{'M','r','%','2','0','J','%', '2', '0', 'S'}), CharBuffer.wrap(sOut));
 	}
 }
--- Cracking/src/chapter1/URLify.java
+++ Cracking/src/chapter1/URLify.java
@@ -1,5 +1,7 @@
 package chapter1;
 
+import java.lang.Character;
+
 /**
  * Replace all spaces inside a string with a %20
  * 
@@ -11,7 +13,7 @@ package chapter1;
 public class URLify {
 
 	/**
-	 * Takes in a string and replaces all occurences of spaces
+	 * Takes in a string and replaces all occurrences of spaces using a string buffer
 	 * with a %20.
 	 * @param s
 	 * @return
@@ -30,6 +32,37 @@ public class URLify {
 		}
 		
 		return sBOut.toString();
+	}
+	
+	/**
+	 * Takes in a string and replaces all occurrences of spaces using char array
+	 * with a %20.
+	 * @param charArr
+	 * @return
+	 */
+	public char[] urlify(char[] charArr) {
+		
+		int insertIndex = charArr.length - 1;
+		boolean firstCharFound = false; 
+		
+		for(int runnerIndex = charArr.length-1 ;runnerIndex >= 0; runnerIndex--) {
+			
+			if(Character.isWhitespace(charArr[runnerIndex]) && !firstCharFound) {
+				continue;
+			} else if(Character.isWhitespace(charArr[runnerIndex])) {
+				charArr[insertIndex] = '0';
+				charArr[insertIndex-1] = '2';
+				charArr[insertIndex-2] = '%';
+				
+				insertIndex -= 3;
+			} else {
+				firstCharFound = true;
+				charArr[insertIndex] = charArr[runnerIndex];
+				insertIndex--;
+			}
+		}
+		
+		return charArr;
 	}
 	
 }
